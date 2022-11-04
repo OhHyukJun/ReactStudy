756e21ae103b5329782e250279af2084acfd8475
OhHyukJun <ohj3961@gmail.com>
2022-11-04 15:24:31 +0900

    initial commit

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,35 @@
+import { useState, useCallback } from 'react';
+import { BsPlus } from 'react-icons/bs';
+import './TodoInsert.scss';
+
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+const onChange = useCallback(e => {
+    setValue(e.target.value);
+}, []);
+
+const onSubmit = useCallback(
+    e=> {
+        onInsert(value);
+        setValue('');
+        e.preventDefault();//새로고침을 발생시키지 않기위해 추가
+    },
+    [onInsert,value],
+);
+
+    return (
+        <form className = "TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder = "할 일을 입력하세요"
+                value={value}
+                onChange={onChange}    
+            />
+            <button type="submit">
+                <BsPlus/>
+            </button>
+        </form>
+    );
+};
+
+export default TodoInsert;
